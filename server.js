e5719829d6a5e60d19cb2684c62251864cb6406f
foamy <martin.red.202@gmail.com>
2020-02-21 02:46:07 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
+
 const PORT = process.env.PORT || 5000
 const apiKey = process.env.apiKey 
 const express = require('express');
@@ -9,8 +10,6 @@ const axios = require('axios');
 
 app.use(express.json());
 app.use(express.static('public'));
-
-
 
 app.post('/weather', (req, res) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${req.body.latitude}&lon=${req.body.longitude}&appid=${apiKey}`
